62a2fbe2d30f67e942b07497b69866fb0b8a7aff
fdominguez <fdominguez@grupocemico.com.ar>
2022-09-01 16:04:18 -0300

    Detallito

--- src/modules/antecedentes-vacunas/entity/vacunas.entity.ts
+++ src/modules/antecedentes-vacunas/entity/vacunas.entity.ts
@@ -8,6 +8,8 @@ import {
 import { antecedentes } from '../../antecedentes/entity/antecedentes.entity';
 import { paciente } from '../../paciente/entity/paciente.entity';
 
+/* Despues de todo esta clase no representa un antecedente si no mas bien un recurso,
+de modo que lo ideal seria que este relacionado directamente al usuario en lugar de a los antecedentes*/
 @Entity('vacunas')
 export class antecedentesVacunas {
   @PrimaryGeneratedColumn()
